a76c965c7230d41a3a81391eb213bafc20a3bafc
Sinan <Sinan@DESKTOP-LV2343J>
2024-02-11 01:46:59 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+            Person person1 = new Person();
+            person1.FirstName = "sinan";
+            person1.LastName = "nalçacı";
+            person1.DateOfBirthYear=1985;
+            person1.NationalIdentity = 123;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+
+        }
+    }
+}
